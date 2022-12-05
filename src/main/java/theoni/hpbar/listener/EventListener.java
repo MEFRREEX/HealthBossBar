9728193b8ef9d5ab0490684cea6d29099d76eb91
Arkadiy <83061703+MEFRREEX@users.noreply.github.com>
2022-12-05 11:07:22 +0200

    Update EventListener.java

--- src/main/java/theoni/hpbar/listener/EventListener.java
+++ src/main/java/theoni/hpbar/listener/EventListener.java
@@ -1,5 +1,6 @@
 package theoni.hpbar.listener;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,15 +23,27 @@ public class EventListener implements Listener {
 
     Main plugin;
     Config config;
+    Config mobs;
     private HashMap<UUID, Long> lastHit = new HashMap<UUID, Long>();
 
     public EventListener(Main plugin) {
         this.plugin = plugin;
         this.config = plugin.getConfig();
+        this.mobs = new Config(new File(plugin.getDataFolder() + "/mobs.yml"), Config.YAML);
     }
     
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
+        // Mob filter
+        if (mobs.getBoolean("enable")) {
+            if (mobs.getString("type").equals("blacklist")) {   
+                if (mobs.getIntegerList("mobs").contains(event.getEntity().getNetworkId())) return;
+            } else if (mobs.getString("type").equals("whitelist")) {
+                if (!mobs.getIntegerList("mobs").contains(event.getEntity().getNetworkId())) return;
+            }
+        }
+
+        // Creating a boss bar
         if (event.getDamager() instanceof Player) {
             Entity entity = event.getEntity();
             Player damager = (Player) event.getDamager();
@@ -52,7 +65,7 @@ public class EventListener implements Listener {
             BossBarManager.addBossBar(damager, bar);
             lastHit.put(damager.getUniqueId(), System.currentTimeMillis());
 
-            // Удаление бара через 5 секунд после удара по игроку
+            // Removal of the boss bar after the time specified in the config.
             (new Timer()).schedule(new TimerTask() {
                 public void run() {
                     if (System.currentTimeMillis() - lastHit.get(damager.getUniqueId()) >= config.getInt("bossbar-remove-time") * 1000) {
