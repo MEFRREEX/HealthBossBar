c883fec0530ee3b587cbee01972610f4120e968d
Arkadiy <83061703+MEFRREEX@users.noreply.github.com>
2022-12-06 05:16:02 +0200

    Reload command registered

--- src/main/java/theoni/hpbar/Main.java
+++ src/main/java/theoni/hpbar/Main.java
@@ -1,14 +1,17 @@
 package theoni.hpbar;
 
+import cn.nukkit.command.Command;
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 import theoni.hpbar.listener.*;
+import theoni.hpbar.commands.*;
 
 public class Main extends PluginBase implements Listener {
 
     public void onEnable() {
-        saveDefaultConfig();
-        saveResource("mobs.yml");
-        getServer().getPluginManager().registerEvents((Listener)new EventListener(this), (Main)this);
+        this.saveDefaultConfig();
+        this.saveResource("mobs.yml");
+        this.getServer().getPluginManager().registerEvents((Listener)new EventListener(this), (Main)this);
+        this.getServer().getCommandMap().register("help", (Command) new ReloadCommand(this));
     }
 }
